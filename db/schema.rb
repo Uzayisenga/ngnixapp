cc21a0c6231ce47bb3f56bbca05355dd84cd3501
Amina <aminauzayisenga@gmail.com>
2019-11-05 15:38:44 +0200

    Update gemfile.lock for heroku

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2019_11_05_115638) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "posts", force: :cascade do |t|
     t.text "name"
     t.string "content"
